e06d838bc9912a68e97d76d0aa72a0064261ef1f
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2019-02-19 17:49:29 +0100

    Added test for dorrBreached

--- DoorControlSolutionManual/DoorControl.NSubstitute.Test/DoorControlDoorBreachedTests.cs
+++ DoorControlSolutionManual/DoorControl.NSubstitute.Test/DoorControlDoorBreachedTests.cs
@@ -13,20 +13,27 @@ namespace DoorControl.NSubstitute.Test
     public class DoorControlDoorBreachedTests
     {
         private DoorControl _uut;
-        private IDoorControlFactory _doorControlFactory;
+        private IDoor _door;
+        private IAlarm _alarm;
+        private IUserValidation _userValidation;
+        private IEntryNotification _entryNotification;
+
 
         [SetUp]
         public void Setup()
         {
-            _doorControlFactory = new NSubstituteDoorControlFactory();
-            _uut = new DoorControl(_doorControlFactory);
+            _alarm = Substitute.For<IAlarm>();
+            _door = Substitute.For<IDoor>();
+            _userValidation = Substitute.For<IUserValidation>();
+            _entryNotification = Substitute.For<IEntryNotification>();
+            _uut = new DoorControl(_userValidation, _door, _entryNotification, _alarm);
         }
 
         [Test]
         public void DoorBreached_DoorStateIsBreached()
         {
             _uut.DoorOpened();
-            Assert.That(_doorControlFactory.CreateAlarm().Equals(false));
+            _alarm.Received(1).SoundAlarm();
         }
 /*
 
