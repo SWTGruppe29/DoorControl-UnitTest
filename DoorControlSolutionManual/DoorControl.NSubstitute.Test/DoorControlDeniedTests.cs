61014f8f5f9a7c00e753b39989e5e58d408a0371
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2019-02-19 13:13:10 +0100

    Added first test

--- DoorControlSolutionManual/DoorControl.NSubstitute.Test/DoorControlDeniedTests.cs
+++ DoorControlSolutionManual/DoorControl.NSubstitute.Test/DoorControlDeniedTests.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DoorControl.Test.Unit.Mocks;
+using NUnit.Framework;
 
 namespace DoorControl.NSubstitute.Test
 {
+    [TestFixture]
     class DoorControlDeniedTests
     {
+        private DoorControl _uut;
+        private MockDoorControlFactory _mockFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockFactory = new MockDoorControlFactory();
+            _uut = new DoorControl(_mockFactory);
+        }
+
+        [Test]
+        public void DoorBreached_DoorStateIsBreached()
+        {
+
+        }
     }
 }
